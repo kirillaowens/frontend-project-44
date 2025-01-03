bed06d4859a7194a8009e0a95fed86842ee53c55
kirillaowens <workmailk3@gmail.com>
2025-01-03 15:13:26 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from "readline-sync";
 
-const userName = () => { 
-    readlineSync.question('May I have your name? '); 
+const doGreeting = () => { // функция приветствия с пользователем
+    const userName = readlineSync.question('May I have your name? '); 
     console.log('Hi ' + userName + '!');
 };
 
-export default userName;
+export default doGreeting;
